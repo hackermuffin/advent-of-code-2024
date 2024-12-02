0be25b4031857c38bc0af838f391a87dd22e9fb7
Peter Oslington <peter.oslington@anu.edu.au>
2024-12-03 00:45:33 +1100

    Added day 1 solution

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,3 +1,42 @@
 pub fn run(input: String) {
-    print!("{}", input)
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    // Parse out left and right arrays
+    input
+        .lines()
+        .map(
+            |line| match line.split(" ").filter(|x| x != &"").collect::<Vec<&str>>()[..] {
+                [l, r] => {
+                    let l = match str::parse::<u32>(l) {
+                        Err(why) => panic!("Could not parse {}: {}", l, why),
+                        Ok(l) => l,
+                    };
+                    let r = match str::parse::<u32>(r) {
+                        Err(why) => panic!("Could not parse {}: {}", r, why),
+                        Ok(r) => r,
+                    };
+                    left.push(l);
+                    right.push(r);
+                }
+                _ => {
+                    println!("Skipping reading in line: {line}");
+                }
+            },
+        )
+        .for_each(drop);
+
+    left.sort();
+    right.sort();
+
+    let mut total: i64 = 0;
+    std::iter::zip(left, right)
+        .map(|(l, r)| {
+            let diff: i64 = (r as i64) - (l as i64);
+            let diff: i64 = diff.abs();
+            total += diff
+        })
+        .for_each(drop);
+
+    println!("Total distance is {total}")
 }
