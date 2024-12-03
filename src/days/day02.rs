58a196fd619d204108a30f0392919b74b18e1060
Peter Oslington <peter.oslington@anu.edu.au>
2024-12-03 15:35:36 +1100

    Cleaned up day 2 code

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -7,31 +7,25 @@ pub fn run(input: String) {
         Ok(parsed) => parsed,
     };
 
-    let safe = parsed.iter().map(is_safe).filter(|x| *x).count();
-    println!("{}", safe);
+    let safe = parsed.iter().map(|x| is_safe(x)).filter(|x| *x).count();
+    println!("Safe plans: {safe}");
 }
 
-fn is_safe(plan: &Vec<u32>) -> bool {
+fn is_safe(plan: &[u32]) -> bool {
     if plan.is_empty() {
         return false;
     };
 
-    println!("{plan:?}");
-    let mut forward = plan.clone();
-    let mut rev = plan.clone();
+    let mut forward = plan.to_vec();
+    let mut rev = forward.clone();
     rev.reverse();
 
     if plan.is_sorted() || rev.is_sorted() {
-        let mut other = plan.clone();
+        let mut other = forward.clone();
         other.rotate_left(1);
         other.pop();
         forward.pop();
 
-        let diff: Vec<_> = other
-            .iter()
-            .zip(forward.clone())
-            .map(|(x, y)| (((*x) as i64) - (y as i64)).abs())
-            .collect();
         let safe = other
             .iter()
             .zip(forward)
