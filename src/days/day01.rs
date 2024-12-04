7e1d6278293ebf8f752951c1c5a47f42c6b54d67
Peter Oslington <peter.oslington@anu.edu.au>
2024-12-04 19:22:49 +1100

    Added part 2 of day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,4 +1,4 @@
-use core::panic;
+use std::collections::HashMap;
 
 use crate::shared::*;
 
@@ -28,7 +28,7 @@ pub fn run(input: String) {
 
     // Total up distances
     let mut total: i64 = 0;
-    std::iter::zip(left, right)
+    std::iter::zip(left.clone(), right.clone())
         .map(|(l, r)| {
             let diff: i64 = (r as i64) - (l as i64);
             let diff: i64 = diff.abs();
@@ -36,5 +36,22 @@ pub fn run(input: String) {
         })
         .for_each(drop);
 
-    println!("Total distance is {total}")
+    println!("Total distance is {total}");
+
+    let mut lfreq = HashMap::new();
+
+    left.iter()
+        .map(|x| lfreq.insert(*x, freq(*x, &right)))
+        .for_each(drop);
+
+    let sim: usize = left
+        .iter()
+        .map(|x| *x as usize * *lfreq.get(x).expect("Failed to find frequency for {x}"))
+        .sum();
+
+    println!("Similarity: {sim}");
+}
+
+fn freq(target: u32, list: &[u32]) -> usize {
+    list.iter().filter(|&&x| x == target).count()
 }
