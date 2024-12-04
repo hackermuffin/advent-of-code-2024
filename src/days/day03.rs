49080a7e6d48cc12c9790ca4476e6e25347a7b42
Peter Oslington <peter.oslington@anu.edu.au>
2024-12-04 19:52:15 +1100

    Added implementation of day 3 pt 1

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,3 +1,16 @@
 pub fn run(input: String) {
-    print!("{}", input)
+    let split = input.split("mul(");
+    let mut parsed = split.map(parse_mul).collect::<Vec<_>>();
+    parsed.retain(|x| x.is_some());
+    let sum: u32 = parsed.iter().map(|x| x.unwrap()).sum();
+    println!("Multiply sum: {sum}");
+}
+
+// Parse mul extracting multiplied value, or nothing if the string is invalid
+fn parse_mul(val: &str) -> Option<u32> {
+    let vals = val.split(")").next()?;
+    let mut split = vals.split(",");
+    let l = str::parse::<u32>(split.next()?).ok()?;
+    let r = str::parse::<u32>(split.next()?).ok()?;
+    Some(l * r)
 }
