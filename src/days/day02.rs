8c8df816007ce888970076b955c54367acb96e28
Peter Oslington <peter.oslington@anu.edu.au>
2024-12-04 19:33:32 +1100

    Added part 2 of day 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -9,6 +9,12 @@ pub fn run(input: String) {
 
     let safe = parsed.iter().map(|x| is_safe(x)).filter(|x| *x).count();
     println!("Safe plans: {safe}");
+    let safe_dampened = parsed
+        .iter()
+        .map(|x| is_safe_dampened(x))
+        .filter(|x| *x)
+        .count();
+    println!("Safe plans with dampening: {safe_dampened}");
 }
 
 fn is_safe(plan: &[u32]) -> bool {
@@ -35,6 +41,22 @@ fn is_safe(plan: &[u32]) -> bool {
             == 0;
 
         return safe;
+    }
+
+    false
+}
+
+fn is_safe_dampened(plan: &[u32]) -> bool {
+    if is_safe(plan) {
+        return true;
+    }
+
+    for i in 0..plan.len() {
+        let mut dampened = plan.to_vec();
+        dampened.remove(i);
+        if is_safe(&dampened) {
+            return true;
+        }
     }
 
     false
