82f8470a41d814fcbe5eb11ccdb4fa9112c6087f
Peter Oslington <peter.oslington@anu.edu.au>
2024-12-06 14:01:59 +1100

    Added day3 pt 2

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,9 +1,18 @@
 pub fn run(input: String) {
+    println!("Multiply sum: {}", sum_all_multiplies(&input));
+
+    // Part 2
+    let do_split = input.split("do()");
+    let trimmed = do_split.map(|x| x.split("don't()").next().unwrap());
+    let sum: u32 = trimmed.map(sum_all_multiplies).sum();
+    println!("Multiply sum with control flow: {sum}");
+}
+
+fn sum_all_multiplies(input: &str) -> u32 {
     let split = input.split("mul(");
     let mut parsed = split.map(parse_mul).collect::<Vec<_>>();
     parsed.retain(|x| x.is_some());
-    let sum: u32 = parsed.iter().map(|x| x.unwrap()).sum();
-    println!("Multiply sum: {sum}");
+    parsed.iter().map(|x| x.unwrap()).sum()
 }
 
 // Parse mul extracting multiplied value, or nothing if the string is invalid
